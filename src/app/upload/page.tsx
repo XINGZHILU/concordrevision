dba26900aaea9db7913f60c62003238314873f2b
XINGZHILU <xingzhi.lu@outlook.com>
2025-02-05 21:14:38 +0000

    Upload page protected

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,7 +1,14 @@
 import {prisma} from "@/lib/prisma";
 import SubjectList from "@/lib/ui/SubjectsList";
+import { currentUser } from '@clerk/nextjs/server'
 
 export default async function Page() {
+    const user = await currentUser();
+
+    if (!user) {
+        return <h1>You must login to access this page</h1>;
+    }
+    
     const subjects = await prisma.subject.findMany();
 
     return (<div>
