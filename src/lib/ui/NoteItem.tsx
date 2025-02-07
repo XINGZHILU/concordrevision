dcedb14179a28ddd01b440dbb1316d4a55bd3882
XINGZHILU <xingzhi.lu@outlook.com>
2025-02-07 15:13:32 +0000

    UI

--- src/lib/ui/NoteItem.tsx
+++ src/lib/ui/NoteItem.tsx
@@ -2,10 +2,10 @@
 
 import Image from 'next/image'
 import React from 'react'
-import {assets} from '@/lib/assets'
+import { assets } from '@/lib/assets'
 import Link from 'next/link'
 
-export function NoteItem({subject}: {
+export function NoteItem({ subject }: {
     subject: {
         id: number,
         title: string,
@@ -15,13 +15,15 @@ export function NoteItem({subject}: {
 }) {
 
     return (
-        <div
-            className='w-[330px] sm:w-[300px] h-[120px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
-            <div className='p-5'>
-                <h5 className='mb-2 text-lg font-medium tracking-tight text-gray-900'>{subject.title}</h5>
-                <Link href={`/revision/${subject.id}`} className='inline-flex items-center py-2 font-semibold text-center'>
-                    View <Image src={assets.arrow} className='ml-2' alt='Arrow' width={12} height={12}/>
-                </Link>
+        <div className='p-4'>
+            <div
+                className='w-[330px] sm:w-[300px] h-[120px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
+                <div className='p-5'>
+                    <h5 className='mb-2 text-lg font-medium tracking-tight text-gray-900'>{subject.title}</h5>
+                    <Link href={`/revision/${subject.id}`} className='inline-flex items-center py-2 font-semibold text-center'>
+                        View <Image src={assets.arrow} className='ml-2' alt='Arrow' width={12} height={12} />
+                    </Link>
+                </div>
             </div>
         </div>
     );
