23b43f30d53f1cf1a9f3c545fc3f061d335d7e46
XINGZHILU <xingzhi.lu@outlook.com>
2025-02-07 13:37:16 +0000

    Update page.tsx

--- src/app/olympiads/page.tsx
+++ src/app/olympiads/page.tsx
@@ -2,7 +2,11 @@ import {prisma} from "@/lib/prisma";
 import OlympiadsList from "@/lib/ui/olympiadslist";
 
 export default async function Page(){
-    const olympiads = await prisma.olympiad.findMany();
+    const olympiads = await prisma.olympiad.findMany({
+        orderBy: {
+            title: 'asc'
+        }
+    });
     return <div className={'content-center'}>
         <h1>Olympiads</h1>
         <OlympiadsList olympiads={olympiads}/>
