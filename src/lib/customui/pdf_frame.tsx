44d4dae63e39278d7ea9f85c8419b88a1beca49f
XINGZHILU <xingzhi.lu@outlook.com>
2025-02-09 14:41:11 +0000

    Update pdf_frame.tsx

--- src/lib/customui/pdf_frame.tsx
+++ src/lib/customui/pdf_frame.tsx
@@ -2,6 +2,6 @@
 
 export default function PDFFrame({url}: { url: string }) {
     return <div className="w-full place-items-center">
-        <iframe itemType={'pdf'} id={'pdf_frame'} src={`https://drive.google.com/viewerng/viewer?embedded=true&url=${url}`} width="100%" height="90%" allowFullScreen={true}/>
+        <iframe itemType={'pdf'} id={'pdf_frame'} src={`https://drive.google.com/viewerng/viewer?embedded=true&url=${url}`} className={'w-11/12 h-screen'} allowFullScreen={true}/>
     </div>;
 }
