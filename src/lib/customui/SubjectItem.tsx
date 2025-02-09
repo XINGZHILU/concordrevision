96ae353b373d0146ecabc09162b7e1a7cd9bc185
XINGZHILU <xingzhi.lu@outlook.com>
2025-02-09 15:33:47 +0000

    Update SubjectItem.tsx

--- src/lib/customui/SubjectItem.tsx
+++ src/lib/customui/SubjectItem.tsx
@@ -4,13 +4,15 @@ import Image from 'next/image'
 import React from 'react'
 import { assets } from '@/lib/assets'
 import Link from 'next/link'
+import {year_group_names} from "@/lib/consts";
 
 export function SubjectItem({ subject }: {
     subject: {
         id: number,
         title: string,
         desc: string,
-        level: number
+        level: number,
+        year: number
     }
 }) {
 
@@ -19,7 +21,7 @@ export function SubjectItem({ subject }: {
             <div
                 className='w-[360px] h-[120px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
                 <div className='p-5'>
-                    <h5 className='mb-2 text-lg font-medium tracking-tight text-gray-900'>{subject.title}</h5>
+                    <h5 className='mb-2 text-lg font-medium tracking-tight text-gray-900'>{year_group_names[subject.year]} {subject.title}</h5>
                     <Link href={`/revision/${subject.id}`} className='inline-flex items-center py-2 font-semibold text-center'>
                         View <Image src={assets.arrow} className='ml-2' alt='Arrow' width={12} height={12} />
                     </Link>
