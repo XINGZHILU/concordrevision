1d895dd9e9d18c8600db31855c1fe9dcce4ebb76
XINGZHILU <xingzhi.lu@outlook.com>
2025-02-09 16:44:33 +0000

    Update pdf_frame.tsx

--- src/lib/customui/pdf_frame.tsx
+++ src/lib/customui/pdf_frame.tsx
@@ -5,7 +5,7 @@ export default function PDFFrame({url}: { url: string }) {
     return <div className="w-full place-items-center">
         <div className="pdf">
             <object data={`${url}#toolbar=1&navpanes=0&scrollbar=1&page=1&view=FitW`}
-                    type="application/pdf">
+                    type="application/pdf" className={"w-full h-screen"}>
                 <p>You do not seem to have a functional PDF plug-in for this browser.</p>
             </object>
         </div>
