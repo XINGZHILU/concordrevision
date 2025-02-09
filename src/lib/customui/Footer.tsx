c3fe8b642d492250c02bfa04f7b999182208e94e
XINGZHILU <xingzhi.lu@outlook.com>
2025-02-09 00:45:17 +0000

    Update Footer.tsx

--- src/lib/customui/Footer.tsx
+++ src/lib/customui/Footer.tsx
@@ -5,10 +5,10 @@ import PomodoroActionBar from "@/lib/customui/pomodoro_action_bar";
 
 const Footer = () => {
     return (
-        <footer className="bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800">
+        <footer className="bg-white rounded-lg shadow-sm m-4 border-gray-200">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">All rights reserved. ©Joshua Ng, Xingzhi Lu, Christoph Chan 2025</span>
-                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                <span className="text-sm text-gray-500 sm:text-center">All rights reserved. ©Joshua Ng, Xingzhi Lu, Christoph Chan 2025</span>
+                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
                     <li>
                         <a href="/about" className="hover:underline me-4 md:me-6">About</a>
                     </li>
