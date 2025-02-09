c54a439d5d47c2958e6266c5f07160a36c5d7c03
XINGZHILU <xingzhi.lu@outlook.com>
2025-02-09 15:05:28 +0000

    Suspense

--- src/lib/customui/pdf_frame.tsx
+++ src/lib/customui/pdf_frame.tsx
@@ -1,7 +1,13 @@
 'use client';
 
+import {Suspense} from "react";
+
 export default function PDFFrame({url}: { url: string }) {
     return <div className="w-full place-items-center">
-        <iframe itemType={'pdf'} id={'pdf_frame'} src={`https://drive.google.com/viewerng/viewer?embedded=true&url=${url}`} className={'w-11/12 h-screen'} allowFullScreen={true}/>
+        <Suspense fallback={<div>Loading pdf...</div>}>
+            <iframe itemType={'pdf'} id={'pdf_frame'}
+                    src={`https://drive.google.com/viewerng/viewer?embedded=true&url=${url}`}
+                    className={'w-11/12 h-screen'} allowFullScreen={true}/>
+        </Suspense>
     </div>;
 }
