919676f3c1a0123655cb57acbeaa96b99fd66c1b
XINGZHILU <xingzhi.lu@outlook.com>
2025-02-09 20:44:13 +0000

    Subject tabs

--- src/lib/customui/RevisionSubjectList.tsx
+++ src/lib/customui/RevisionSubjectList.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import {year_group_names} from "@/lib/consts";
-import React, {useState} from 'react'
 
 import {SubjectItem} from './SubjectItem'
+import { Link as CL, Tabs } from "@chakra-ui/react"
 
 const RevisionSubjectList = ({subjects, year}: {
     subjects: {
@@ -13,30 +12,59 @@ const RevisionSubjectList = ({subjects, year}: {
         level: number
     }[], year: number
 }) => {
-
-    if (year < 0 || year > 5){
-        year = 0;
-    }
-    const [menu, setMenu] = useState(year);
+    const years=['f3', 'f4', 'f5', '61', '62'];
+    const f3 = subjects.filter((subject) => subject.level === 0).map((subject) => (
+        <SubjectItem key={subject.title + subject.id} subject={subject}/>)
+    );
+    const f4 = subjects.filter((subject) => subject.level === 1).map((subject) => (
+        <SubjectItem key={subject.title + subject.id} subject={subject}/>)
+    );
+    const f5 = subjects.filter((subject) => subject.level === 2).map((subject) => (
+        <SubjectItem key={subject.title + subject.id} subject={subject}/>)
+    );
+    const f61 = subjects.filter((subject) => subject.level === 3).map((subject) => (
+        <SubjectItem key={subject.title + subject.id} subject={subject}/>)
+    );
+    const f62 = subjects.filter((subject) => subject.level === 4).map((subject) => (
+        <SubjectItem key={subject.title + subject.id} subject={subject}/>)
+    );
 
     return (
-        <div>
-            <div className='flex justify-center gap-6 my-10'>
-                {
-                    year_group_names.map((year, index) => (
-                        <button key={index} onClick={() => setMenu(index)}
-                                className={menu === index ? 'bg-black text-white py-1 px-4 rounded-sm' : ""}>{year}</button>
-                    ))
-                }
-            </div>
-            <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
-                {
-                    subjects.filter((subject) => subject.level===menu).map((subject) => {
-                        return <SubjectItem key={subject.title + subject.id} subject={subject}/>;
-                    })
-                }
-            </div>
-        </div>
+        <Tabs.Root defaultValue={years[year]} variant='plain' rounded="l3">
+            <Tabs.List p="1">
+                <Tabs.Trigger value="f3" asChild p="2">
+                    <CL unstyled href="#f3">
+                        Form 3
+                    </CL>
+                </Tabs.Trigger>
+                <Tabs.Trigger value="f4" asChild p="2">
+                    <CL unstyled href="#f4">
+                        Form 4
+                    </CL>
+                </Tabs.Trigger>
+                <Tabs.Trigger value="f5" asChild p="2">
+                    <CL unstyled href="#f5">
+                        Form 5
+                    </CL>
+                </Tabs.Trigger>
+                <Tabs.Trigger value="61" asChild p="2">
+                    <CL unstyled href="#61">
+                        6.1
+                    </CL>
+                </Tabs.Trigger>
+                <Tabs.Trigger value="62" asChild p="2">
+                    <CL unstyled href="#62">
+                        6.2
+                    </CL>
+                </Tabs.Trigger>
+                <Tabs.Indicator rounded="l2" />
+            </Tabs.List>
+            <Tabs.Content value="f3">{f3}</Tabs.Content>
+            <Tabs.Content value="f4">{f4}</Tabs.Content>
+            <Tabs.Content value="f5">{f5}</Tabs.Content>
+            <Tabs.Content value="61">{f61}</Tabs.Content>
+            <Tabs.Content value="62">{f62}</Tabs.Content>
+        </Tabs.Root>
     )
 }
 
--- src/lib/customui/UploadSubjectsList.tsx
+++ src/lib/customui/UploadSubjectsList.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-import {useState} from "react";
-import {year_group_names} from "@/lib/consts";
 import Link from "next/link";
+import { Link as CL, Tabs } from "@chakra-ui/react"
 
-export default function SubjectList({subjects, year}: {
+export default function SubjectList({ subjects, year }: {
     subjects: {
         id: number,
         title: string,
@@ -12,23 +11,63 @@ export default function SubjectList({subjects, year}: {
         level: number
     }[], year: number
 }) {
-    const [menu, setMenu] = useState(year);
+    const years=['f3', 'f4', 'f5', '61', '62'];
+    const f3 = subjects.filter((subject) => subject.level === 0).map((subject) => (
+        <li key={subject.title + 'li'}><Link key={subject.title + subject.id} className={'border-dotted border-2 border-blue-500'}
+            href={`/upload/revision/${subject.id}`}>{subject.title}</Link></li>)
+    );
+    const f4 = subjects.filter((subject) => subject.level === 1).map((subject) => (
+        <li key={subject.title + 'li'}><Link key={subject.title + subject.id} className={'border-dotted border-2 border-blue-500'}
+            href={`/upload/revision/${subject.id}`}>{subject.title}</Link></li>)
+    );
+    const f5 = subjects.filter((subject) => subject.level === 2).map((subject) => (
+        <li key={subject.title + 'li'}><Link key={subject.title + subject.id} className={'border-dotted border-2 border-blue-500'}
+            href={`/upload/revision/${subject.id}`}>{subject.title}</Link></li>)
+    );
+    const f61 = subjects.filter((subject) => subject.level === 3).map((subject) => (
+        <li key={subject.title + 'li'}><Link key={subject.title + subject.id} className={'border-dotted border-2 border-blue-500'}
+            href={`/upload/revision/${subject.id}`}>{subject.title}</Link></li>)
+    );
+    const f62 = subjects.filter((subject) => subject.level === 4).map((subject) => (
+        <li key={subject.title + 'li'}><Link key={subject.title + subject.id} className={'border-dotted border-2 border-blue-500'}
+            href={`/upload/revision/${subject.id}`}>{subject.title}</Link></li>)
+    );
     return (<div>
-        <div className='flex justify-center gap-6 my-10'>
-            {
-                year_group_names.map((year, index) => (
-                    <button key={index} onClick={() => setMenu(index)}
-                            className={menu === index ? 'bg-black text-white py-1 px-4 rounded-sm' : ""}>{year}</button>
-                ))
-            }
-        </div>
-        <ul>
-            {
-                subjects.filter((subject) => subject.level === menu).map((subject) => (
-                    <li key={subject.title + 'li'}><Link key={subject.title + subject.id} className={'border-dotted border-2 border-blue-500'}
-                                                         href={`/upload/revision/${subject.id}`}>{subject.title}</Link></li>)
-                )
-            }
-        </ul>
+        <Tabs.Root defaultValue={years[year]} variant='plain' rounded="l3">
+            <Tabs.List p="1">
+                <Tabs.Trigger value="f3" asChild p="2">
+                    <CL unstyled href="#f3">
+                        Form 3
+                    </CL>
+                </Tabs.Trigger>
+                <Tabs.Trigger value="f4" asChild p="2">
+                    <CL unstyled href="#f4">
+                        Form 4
+                    </CL>
+                </Tabs.Trigger>
+                <Tabs.Trigger value="f5" asChild p="2">
+                    <CL unstyled href="#f5">
+                        Form 5
+                    </CL>
+                </Tabs.Trigger>
+                <Tabs.Trigger value="61" asChild p="2">
+                    <CL unstyled href="#61">
+                        6.1
+                    </CL>
+                </Tabs.Trigger>
+                <Tabs.Trigger value="62" asChild p="2">
+                    <CL unstyled href="#62">
+                        6.2
+                    </CL>
+                </Tabs.Trigger>
+                <Tabs.Indicator rounded="l2" />
+            </Tabs.List>
+            <Tabs.Content value="f3">{f3}</Tabs.Content>
+            <Tabs.Content value="f4">{f4}</Tabs.Content>
+            <Tabs.Content value="f5">{f5}</Tabs.Content>
+            <Tabs.Content value="61">{f61}</Tabs.Content>
+            <Tabs.Content value="62">{f62}</Tabs.Content>
+        </Tabs.Root>
+
     </div>)
 }
