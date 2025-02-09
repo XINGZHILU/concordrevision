57a19a128a138f905ea1b7e5f52c4c6dc578e4fd
XINGZHILU <xingzhi.lu@outlook.com>
2025-02-09 17:31:59 +0000

    PDF

--- src/lib/customui/pdf_frame.tsx
+++ src/lib/customui/pdf_frame.tsx
@@ -1,13 +1,11 @@
 'use client';
 
+//import {Suspense} from "react";
 
 export default function PDFFrame({url}: { url: string }) {
-    return <div className="w-full place-items-center h-screen">
-        <div className="pdf h-screen w-full" >
-            <object data={`${url}#toolbar=1&navpanes=0&scrollbar=1&page=1&view=FitW`}
-                    type="application/pdf" className={"w-full h-screen"}>
-                <p>You do not seem to have a functional PDF plug-in for this browser.</p>
-            </object>
-        </div>
+    return <div>
+        <iframe itemType={'application/pdf'} id={'pdf_frame'}
+                src={`https://drive.google.com/viewerng/viewer?embedded=true&url=${url}`}
+                className={'w-11/12 h-screen'} allowFullScreen={true}/>
     </div>;
 }
