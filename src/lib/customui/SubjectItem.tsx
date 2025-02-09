13f2c2d576909c28c708c36e53f3316c74a4ba81
XINGZHILU <xingzhi.lu@outlook.com>
2025-02-09 15:37:41 +0000

    Update SubjectItem.tsx

--- src/lib/customui/SubjectItem.tsx
+++ src/lib/customui/SubjectItem.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import { assets } from '@/lib/assets'
 import Link from 'next/link'
 import {year_group_names} from "@/lib/consts";
+import {Badge} from "@chakra-ui/react";
 
 export function SubjectItem({ subject }: {
     subject: {
@@ -12,7 +13,6 @@ export function SubjectItem({ subject }: {
         title: string,
         desc: string,
         level: number,
-        year: number
     }
 }) {
 
@@ -21,7 +21,7 @@ export function SubjectItem({ subject }: {
             <div
                 className='w-[360px] h-[120px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
                 <div className='p-5'>
-                    <h5 className='mb-2 text-lg font-medium tracking-tight text-gray-900'>{year_group_names[subject.year]} {subject.title}</h5>
+                    <h5 className='mb-2 text-lg font-medium tracking-tight text-gray-900'>{subject.title} <Badge colorPalette={'purple'}>{year_group_names[subject.level]}</Badge></h5>
                     <Link href={`/revision/${subject.id}`} className='inline-flex items-center py-2 font-semibold text-center'>
                         View <Image src={assets.arrow} className='ml-2' alt='Arrow' width={12} height={12} />
                     </Link>
