64caece121977df67124c33fed4292829dbdc196
XINGZHILU <xingzhi.lu@outlook.com>
2025-02-09 23:18:27 +0000

    Update filelist.tsx

--- src/lib/customui/filelist.tsx
+++ src/lib/customui/filelist.tsx
@@ -14,6 +14,8 @@ export default function FileList({files}: {
                     <a target="_blank" href={file.path}
                        className={'p-2 border-solid border-gray-500 border-2 rounded-lg'}>{file.filename}</a>
                 </h3>
+                <br/>
+                
             </div>;
         })}
     </div>);
