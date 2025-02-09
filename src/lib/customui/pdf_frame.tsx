3bd591e982ba5bffd4ab53c36de60f0b7b1559ee
XINGZHILU <xingzhi.lu@outlook.com>
2025-02-09 15:07:46 +0000

    Update pdf_frame.tsx

--- src/lib/customui/pdf_frame.tsx
+++ src/lib/customui/pdf_frame.tsx
@@ -4,7 +4,7 @@ import {Suspense} from "react";
 
 export default function PDFFrame({url}: { url: string }) {
     return <div className="w-full place-items-center">
-        <Suspense fallback={<div>Loading pdf...</div>}>
+        <Suspense fallback={<p>Loading pdf...</p>}>
             <iframe itemType={'pdf'} id={'pdf_frame'}
                     src={`https://drive.google.com/viewerng/viewer?embedded=true&url=${url}`}
                     className={'w-11/12 h-screen'} allowFullScreen={true}/>
