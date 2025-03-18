e204eeba5f0993c22c004c934570c6f2776ce4ae
XINGZHILU <xingzhi.lu@outlook.com>
2025-03-18 18:55:13 +0000

    Update new_post.tsx

--- src/lib/customui/EC/new_post.tsx
+++ src/lib/customui/EC/new_post.tsx
@@ -46,16 +46,15 @@ export default function PostForm({author}: { author: string }) {
         setValue("");
     }
 
+    // @ts-expect-error
     return (
         <div className="container w-full">
             <form onSubmit={submit}>
                 <label htmlFor={'title'}>Title: </label>
                 <input type="text" ref={titleRef} name={'title'} placeholder={'Enter the title of the post'}
                        required={true}/> <br/>
-                <MDEditor
-                    value={value}
-                    onChange={setValue}
-                />
+                
+                <MDEditor value={value} onChange={setValue}/>
                 <Button colorPalette={'blue'} type="submit">Post</Button>
             </form>
             <Alert.Root status="warning">
