7c8fac8678c63d48b80b1c1902ee396cfc5e332a
XINGZHILU <xingzhi.lu@outlook.com>
2025-03-23 19:27:48 +0000

    Update pdf_frame.tsx

--- src/lib/customui/Basic/pdf_frame.tsx
+++ src/lib/customui/Basic/pdf_frame.tsx
@@ -1,9 +1,14 @@
-export default function PdfViewer({ url, title } : {
+export default function PdfViewer({url, title}: {
     url: string;
     title: string;
 }) {
     return <div>
         <h1>{title}</h1>
-        <iframe src={url} className={'w-full h-screen'}></iframe>
+        <iframe src={url} style={
+            {
+                width: "100%",
+                height: "100%"
+            }
+        }></iframe>
     </div>;
 }
