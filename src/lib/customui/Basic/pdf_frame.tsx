e6733e654fbbdfb3da99022ebb49202203776cb2
XINGZHILU <xingzhi.lu@outlook.com>
2025-03-23 21:14:56 +0000

    Update pdf_frame.tsx

--- src/lib/customui/Basic/pdf_frame.tsx
+++ src/lib/customui/Basic/pdf_frame.tsx
@@ -2,6 +2,7 @@ export default function PdfViewer({url, title}: {
     url: string;
     title: string;
 }) {
+    url = url.replace(/ /g, '%20');
     return <div className={'min-h-screen h-full w-full'}>
         <iframe src={`https://docs.google.com/viewer?url=${url}&embedded=true`} style={
             {
