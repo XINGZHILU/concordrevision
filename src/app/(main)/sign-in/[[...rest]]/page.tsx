6d85ab29e90b65067bf16af10c91425eb54107be
XINGZHILU <xingzhi.lu@outlook.com>
2025-04-06 01:07:37 +0100

    Update page.tsx

--- src/app/(main)/sign-in/[[...rest]]/page.tsx
+++ src/app/(main)/sign-in/[[...rest]]/page.tsx
@@ -1,7 +1,7 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-    return <div className={'place-items-center'}>
+    return <div className={'place-items-center w-screen'}>
         <SignIn />
     </div>
 }
