170eeff2fab1817248c6132337e8a9efbcfc0ab6
XINGZHILU <xingzhi.lu@outlook.com>
2025-04-06 22:24:38 +0100

    Removed public access

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,4 @@
+/*
 import {clerkMiddleware, ClerkMiddlewareAuth, createRouteMatcher} from '@clerk/nextjs/server';
 import {NextResponse, NextRequest} from "next/server";
 import {get} from '@vercel/edge-config';
@@ -18,6 +19,31 @@ export default clerkMiddleware(async (auth: ClerkMiddlewareAuth, request: NextRe
 
     // Then handle authentication
     if (isPrivateRoute(request)) {
+        await auth.protect();
+    }
+
+    return NextResponse.next();
+});
+
+export const config = {
+    matcher: ['/((?!_next|.*\\..*).*)', '/api/:path*'],
+};
+*/
+
+import { clerkMiddleware, ClerkMiddlewareAuth } from '@clerk/nextjs/server';
+import { NextResponse, NextRequest } from "next/server";
+import { get } from '@vercel/edge-config';
+
+export default clerkMiddleware(async (auth: ClerkMiddlewareAuth, request: NextRequest) => {
+    // Check maintenance mode first
+    const isInMaintenanceMode = await get('maintenance');
+    if (isInMaintenanceMode) {
+        request.nextUrl.pathname = `/maintenance`;
+        return NextResponse.rewrite(request.nextUrl);
+    }
+
+    // Then handle authentication
+    if (!/^\/sign-in(?:\/.*)?$/.test(request.nextUrl.pathname)) {
         await auth.protect();
     }
 
