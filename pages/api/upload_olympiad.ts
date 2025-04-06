7e573d3b1771f68e7e75b99dbb7b5e3396e46ac9
XINGZHILU <xingzhi.lu@outlook.com>
2025-04-06 11:22:21 +0100

    Update upload_olympiad.ts

--- pages/api/upload_olympiad.ts
+++ pages/api/upload_olympiad.ts
@@ -1,10 +1,27 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "@/lib/prisma";
+import { getAuth } from "@clerk/nextjs/server";
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse,
 ) {
+    const { userId } = getAuth(req);
+
+    if (!userId) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const record = await prisma.user.findUnique({
+        where: {
+            id : userId,
+        }
+    });
+
+    if (!record){
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+
     const urls = req.body.urls as string[];
     const title = req.body.title as string;
     const desc = req.body.desc as string;
@@ -12,12 +29,13 @@ export default async function handler(
     const author = req.body.author as string;
     const names = req.body.names as string[];
 
-    const o_r = await prisma.olympiad_Resource.create({
+    const resource = await prisma.olympiad_Resource.create({
         data: {
             title: title,
             olympiadId: olympiad,
             desc: desc,
-            authorId: author
+            authorId: author,
+            approved: record.teacher
         }
     });
 
@@ -26,7 +44,7 @@ export default async function handler(
             data: {
                 filename: names[i],
                 path: urls[i],
-                olympiadId: o_r.id,
+                olympiadId: resource.id,
             }
         });
     }
