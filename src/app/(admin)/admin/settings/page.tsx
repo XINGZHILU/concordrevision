bac016a918aa3430140050a1a3219c0969c3a60c
XINGZHILU <xingzhi.lu@outlook.com>
2025-04-06 01:44:46 +0100

    Update page.tsx

--- src/app/(admin)/admin/settings/page.tsx
+++ src/app/(admin)/admin/settings/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
 
 
     return <div>
-        <center>
+        <center className={'min-h-screen'}>
             <h1>The settings page will be added in a future update</h1>
         </center>
     </div>;
