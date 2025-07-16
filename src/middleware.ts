54ca42d41800dcebd2d42ba98d48d3b82c3335de
XINGZHILU <xingzhi.lu@outlook.com>
2025-07-16 23:30:55 +0800

    Banned visiting without signin

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,10 +5,11 @@ import {get} from '@vercel/edge-config';
 const isPrivateRoute = createRouteMatcher([
     '/upload(.*)',
     '/admin(.*)',
-])
-
-const isPublicRoute = createRouteMatcher([
-    '/api/webhooks/clerk(.*)',
+    '/teachers(.*)',
+    '/olympiads(.*)',
+    '/revision(.*)',
+    '/ucas(.*)',
+    '/',
 ])
 
 
@@ -18,10 +19,6 @@ export default clerkMiddleware(async (auth: ClerkMiddlewareAuth, request: NextRe
     if (isInMaintenanceMode && process.env.ONLINE === 'true') {
         request.nextUrl.pathname = `/maintenance`;
         return NextResponse.rewrite(request.nextUrl);
-    }
-
-    if (isPublicRoute(request)) {
-        return NextResponse.next();
     }
 
     // Then handle authentication
