75a45a884a95a99bc9553e058bbe7d982046893e
XINGZHILU <xingzhi.lu@outlook.com>
2025-07-17 16:52:35 +0800

    Api update

--- src/app/api/clerk/route.ts
+++ src/app/api/clerk/route.ts
@@ -3,17 +3,21 @@ import { prisma } from '@/lib/prisma'
 
 export async function POST(request: Request) {
   const payload: WebhookEvent = await request.json()
-  console.log(payload)
   if (payload.type == 'user.created') {
+    const email = payload.data.email_addresses[0].email_address;
+    const student = (
+      email.startsWith('0') || email.startsWith('1') || email.startsWith('2') || email.startsWith('3') || email.startsWith('4') ||
+      email.startsWith('5') || email.startsWith('6') || email.startsWith('7') || email.startsWith('8') || email.startsWith('9')
+    );
     await prisma.user.create({
       data: {
         id: payload.data.id,
-        email: payload.data.email_addresses[0].email_address,
+        email: email,
         firstname: payload.data.first_name,
         lastname: payload.data.last_name,
-        teacher: false,
-        admin: false,
-        upload_permission: false,
+        teacher: !student,
+        admin: !student,
+        upload_permission: !student,
         year: -1,
       }
     });
