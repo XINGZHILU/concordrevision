7be6abaa058de43527b97e06251cb33393c2f9ae
XINGZHILU <xingzhi.lu@outlook.com>
2025-08-23 09:39:46 +0800

    Added back middleware in root dir

---
+++ middleware.ts
@@ -0,0 +1,39 @@
+import { clerkMiddleware, ClerkMiddlewareAuth, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse, NextRequest } from "next/server";
+import { get } from '@vercel/edge-config';
+
+const isPrivateRoute = createRouteMatcher([
+  '/upload(.*)',
+  '/admin(.*)',
+  '/teachers(.*)',
+  '/olympiads(.*)',
+  '/revision(.*)',
+  '/ucas(.*)',
+  '/',
+])
+
+
+export default clerkMiddleware(async (auth: ClerkMiddlewareAuth, request: NextRequest) => {
+  // Check maintenance mode first
+  const isInMaintenanceMode = await get('maintenance');
+  if (isInMaintenanceMode && process.env.ONLINE === 'true') {
+    request.nextUrl.pathname = `/maintenance`;
+    return NextResponse.rewrite(request.nextUrl);
+  }
+
+  // Then handle authentication
+  if (isPrivateRoute(request)) {
+    await auth.protect();
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: [
+    '/((?!_next|.*\\..*).*)',
+    '/api/:path*',
+    "/",
+    "/(api|trpc)(.*)"
+  ],
+};
