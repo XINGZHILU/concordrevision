f4f11367c1b4826ae11009d6b43b85a149baa0a0
XINGZHILU <xingzhi.lu@outlook.com>
2025-10-28 15:30:42 +0000

    Update in terms layout

--- src/app/(terms)/layout.tsx
+++ src/app/(terms)/layout.tsx
@@ -1,12 +1,11 @@
 import "@/app/globals.css";
 import "@/app/md.css";
-import Footer from "@/lib/customui/Footer";
-
+import Link from "next/link";
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (<>
     {children}
-    <br />
-    <Footer />
+    <br/>
+    <Link href={'/'}>Back to home page</Link>
   </>);
 }
