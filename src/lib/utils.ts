2f1ed186557d5db8a27696dcbfaf42c01e2b5681
XINGZHILU <xingzhi.lu@outlook.com>
2025-11-05 20:53:02 +0000

    Redirecting fixed

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -29,7 +29,7 @@ export function getPdfViewerUrl(fileUrl: string, fileName?: string): string {
         params.set('name', fileName);
     }
     
-    return `/viewer/pdf/view?${params.toString()}`;
+    return `/pdf/view?${params.toString()}`;
 }
 
 /**
