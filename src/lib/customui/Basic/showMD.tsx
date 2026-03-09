c6ae982c95076baa044c78d39084083a46356e76
XINGZHILU <xingzhi.lu@outlook.com>
2026-03-09 13:59:43 +0000

    Itallics rendering

--- src/lib/customui/Basic/showMD.tsx
+++ src/lib/customui/Basic/showMD.tsx
@@ -1,12 +1,14 @@
 'use client';
 
-import MDEditor from "@uiw/react-md-editor";
 import { useTheme } from "next-themes";
-import React from 'react';
+import React from "react";
+import CompleteMarkdownRenderer from "@/lib/customui/Basic/customMDRenderer";
 
 export default function MDViewer({ content }: { content: string }) {
     const { resolvedTheme } = useTheme();
-    return <div className="container" data-color-mode={resolvedTheme}>
-        <MDEditor.Markdown source={content} style={{ background: 'transparent' }} />
-    </div>;
+    return (
+        <div className="container" data-color-mode={resolvedTheme}>
+            <CompleteMarkdownRenderer content={content} />
+        </div>
+    );
 }
